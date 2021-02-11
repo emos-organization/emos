cabc6cffbb0750951e0c4c084c760a0dc730a8e0
sadantange <3026176790@qq.com>
2021-02-11 13:40:58 +0800

    刷脸签到系统

---
+++ emos-wx-api/src/main/java/com/example/emos/wx/service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.emos.wx.service;
+
+import com.example.emos.wx.db.pojo.TbUser;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public interface UserService {
+    public int registerUser(String registerCode,String code,String nickname,String photo);
+
+    public Set<String> searchUserPermissions(int userId);
+
+    public Integer login(String code);
+
+    public TbUser searchById(int userId);
+
+    public String searchUserHiredate(int userId);
+
+    public HashMap searchUserSummary(int userId);
+}
---
+++ emos-wx/main.js
@@ -0,0 +1,67 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+	...App
+})
+app.$mount()
+
+let baseUrl = "http://192.168.99.216:8080/emos-wx-api"
+
+Vue.prototype.url = {
+	register: baseUrl + "/user/register",
+	login: baseUrl + "/user/login",
+	checkin: baseUrl + "/checkin/checkin",
+	createFaceModel: baseUrl + "/checkin/createFaceModel",
+	validCanCheckIn: baseUrl + "/checkin/validCanCheckIn",
+	searchTodayCheckin:baseUrl+"/checkin/searchTodayCheckin",
+	searchUserSummary:baseUrl+"/user/searchUserSummary",
+	searchMonthCheckin:baseUrl+"/checkin/searchMonthCheckin"
+}
+
+Vue.prototype.checkPermission=function(perms){
+	let permission=uni.getStorageSync("permission")
+	let result=false
+	for(let one of perms){
+		if(permission.indexOf(one)!=-1){
+			result=true
+			break
+		}
+	}
+	return result
+}
+
+Vue.prototype.ajax = function(url, method, data, fun) {
+	uni.request({
+		"url": url,
+		"method": method,
+		"header": {
+			token: uni.getStorageSync("token")
+		},
+		"data": data,
+		success: function(resp) {
+			if (resp.statusCode == 401) {
+				uni.redirectTo({
+					url: "/pages/login/login.vue"
+				})
+			} else if (resp.statusCode == 200 && resp.data.code == 200) {
+				let data = resp.data
+				if (data.hasOwnProperty("token")) {
+					let token = data.token
+					console.log(token)
+					uni.setStorageSync("token", token)
+				}
+				fun(resp)
+			} else {
+				uni.showToast({
+					icon: "none",
+					title: resp.data
+				})
+			}
+		}
+	})
+}
