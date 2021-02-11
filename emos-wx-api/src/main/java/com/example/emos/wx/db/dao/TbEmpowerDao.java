906bad9da2f556b4610405d1f12c58d5597bc332
sadantange <3026176790@qq.com>
2021-02-11 22:03:44 +0800

    进一步修改mybatis的xml与Dao文件

--- emos-wx-api/src/main/java/com/example/emos/wx/db/dao/TbEmpowerDao.java
+++ emos-wx-api/src/main/java/com/example/emos/wx/db/dao/TbEmpowerDao.java
@@ -3,15 +3,5 @@ package com.example.emos.wx.db.dao;
 import com.example.emos.wx.db.pojo.TbEmpower;
 
 public interface TbEmpowerDao {
-    int deleteByPrimaryKey(Integer userId);
-
-    int insert(TbEmpower record);
-
-    int insertSelective(TbEmpower record);
-
-    TbEmpower selectByPrimaryKey(Integer userId);
-
-    int updateByPrimaryKeySelective(TbEmpower record);
-
-    int updateByPrimaryKey(TbEmpower record);
+    public Integer searchIdByOpenId(String openId);
 }
--- emos-wx-api/src/main/java/com/example/emos/wx/db/dao/TbUserDao.java
+++ emos-wx-api/src/main/java/com/example/emos/wx/db/dao/TbUserDao.java
@@ -1,17 +1,12 @@
 package com.example.emos.wx.db.dao;
 
 import com.example.emos.wx.db.pojo.TbUser;
+import org.apache.ibatis.annotations.Mapper;
 
+import java.util.HashMap;
+
+@Mapper
 public interface TbUserDao {
-    int deleteByPrimaryKey(Integer userId);
-
-    int insert(TbUser record);
-
-    int insertSelective(TbUser record);
-
-    TbUser selectByPrimaryKey(Integer userId);
-
-    int updateByPrimaryKeySelective(TbUser record);
-
-    int updateByPrimaryKey(TbUser record);
+    public boolean haveRootUser();
+    public int insert(HashMap param);
 }
